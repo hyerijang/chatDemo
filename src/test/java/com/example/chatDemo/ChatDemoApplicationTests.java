b5c4e0d5f8ac809db430481665e72572006a3e4d
Hyeri Jang <chjang5621@naver.com>
2021-09-01 17:11:35 +0900

    Initial commit

---
+++ src/test/java/com/example/chatDemo/ChatDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chatDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
